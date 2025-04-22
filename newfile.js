8676fb8b2f4c48c033b2ab60c5bc58150588b5a5
lokesh <lokeshkskiran@gmail.com>
2025-04-22 18:56:50 +0530

    Added multiplication function

---
+++ newfile.js
@@ -0,0 +1,12 @@
+// Function to multiply two numbers
+function multiplyNumbers(a, b) {
+    return a * b;
+  }
+  
+  // Example usage
+  let num1 = 6;
+  let num2 = 3;
+  let result = multiplyNumbers(num1, num2);
+  
+  console.log("The result is: " + result);
+  
