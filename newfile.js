a6344fac9d0dee8e833142581071c13f880519a9
lokesh <lokeshkskiran@gmail.com>
2025-04-22 18:59:18 +0530

    Added division function

--- newfile.js
+++ newfile.js
@@ -9,4 +9,20 @@ function multiplyNumbers(a, b) {
   let result = multiplyNumbers(num1, num2);
   
   console.log("The result is: " + result);
+ 
+  
+  // Function to divide two numbers
+function divideNumbers(a, b) {
+    if (b === 0) {
+      return "Cannot divide by zero";
+    }
+    return a / b;
+  }
+  
+  // Example usage
+  let num3 = 12;
+  let num4 = 4;
+  let result1= divideNumbers(num3, num4);
+  
+  console.log("The result is: " + result1);
   
